0bc804bf73102e65863e65cc5ea36aee13ddb47a
haider-06418 <ma06418@st.habib.edu.pk>
2023-12-11 21:22:05 +0500

    added functionality for visited nodes count

--- bfs.py
+++ bfs.py
@@ -9,20 +9,22 @@ from collections import deque
 def bfs_algorithm(graph, start, goal):
     queue = deque([start])
     visited = {start: None}  # Track visited nodes and paths
+    nodes_visited = 0  # Counter for the number of visited nodes
 
     while queue:
         current = queue.popleft()
+        nodes_visited += 1
 
         if current == goal:
             path = []
             while current:
                 path.append(current)
                 current = visited[current]
-            return path[::-1]  # Return path in correct order
+            return path[::-1], nodes_visited  # Return path and number of nodes visited
 
         for neighbor in graph[current]:
             if neighbor['city'] not in visited:
                 visited[neighbor['city']] = current
                 queue.append(neighbor['city'])
 
-    return None  # No path found
+    return None, nodes_visited  # No path found, return None for path and the number of nodes visited
--- dfs.py
+++ dfs.py
@@ -6,20 +6,22 @@
 def dfs_algorithm(graph, start, goal):
     stack = [start]
     visited = {start: None}  # Track visited nodes and paths
+    nodes_visited = 0  # Counter for the number of visited nodes
 
     while stack:
         current = stack.pop()
+        nodes_visited += 1
 
         if current == goal:
             path = []
             while current:
                 path.append(current)
                 current = visited[current]
-            return path[::-1]  # Return path in correct order
+            return path[::-1], nodes_visited  # Return path and number of nodes visited
 
         for neighbor in graph[current]:
             if neighbor['city'] not in visited:
                 visited[neighbor['city']] = current
                 stack.append(neighbor['city'])
 
-    return None  # No path found
+    return None, nodes_visited  # No path found, return None for path and the number of nodes visited
