95faedbc7ac68f7f8c615688e831f97914de63ef
haider-06418 <ma06418@st.habib.edu.pk>
2023-12-11 20:48:53 +0500

    minor changes

--- create_graph.py
+++ create_graph.py
@@ -5,7 +5,7 @@ import json
 
 
 # function to load json file into a dictionary
-def load_data_from_json(json_file):
+def load_json(json_file):
     with open(json_file, 'r') as file:
         data = json.load(file)
     return data
@@ -19,12 +19,13 @@ def haversine(lat1, lon1, lat2, lon2):
     a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) ** 2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     distance = R * c
+    # distance = math.sqrt((lat2 - lat1)**2 + (lon2 - lon1)**2) # Euclidean distance
     return distance
 
 
 def create_graph(city_data_file):
     # city name and coordinates for calculating distance
-    city_coordinates = load_data_from_json(city_data_file)
+    city_coordinates = load_json(city_data_file)
 
     # initializing the graph
     graph = {city: [] for city in city_coordinates}
@@ -34,14 +35,20 @@ def create_graph(city_data_file):
         for city2 in city_coordinates:
             if city1 != city2:
                 dist = haversine(*city_coordinates[city1], *city_coordinates[city2])
-                graph[city1].append({'city': city2, 'distance': dist})
+                # if dist <= 250:
+                if dist <= 150:
+                    graph[city1].append({'city': city2, 'distance': dist})
 
     # saving the graph to a json file
-    with open('graph.json', 'w') as file:
+    with open('graphs/graph.json', 'w') as file:
         json.dump(graph, file, indent=4)
-
     print("Graph saved to 'graph.json'")
 
+
+    # for node in graph:    
+    #     # # Check if the value is an empty list
+    #     # if not graph[node]:
+    #     #     print(f"No values for node: {node}")
 
 
 create_graph('city_data.json')
