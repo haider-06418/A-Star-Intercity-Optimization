5590c5fe6090f69ccd46c3305482a5930fea335f
haider-06418 <ma06418@st.habib.edu.pk>
2023-12-11 20:54:16 +0500

    testing

--- a_star.py
+++ a_star.py
@@ -2,9 +2,110 @@
 
 # adapted from: https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
 
+# import heapq
+
+# # a star search algorithm
+# def a_star_search(graph, start, goal):
+#     priority_queue = [(0, start, [])]
+#     visited = set()
+
+#     while priority_queue:
+#         (cost, current_node, path) = heapq.heappop(priority_queue)
+
+#         if current_node in visited:
+#             continue
+
+#         visited.add(current_node)
+#         path = path + [current_node]
+
+#         if current_node == goal:
+#             return path, cost
+
+#         for neighbor in graph[current_node]:
+#             neighbor_city = neighbor['city']
+#             neighbor_cost = neighbor['distance']
+#             total_cost = cost + neighbor_cost
+#             heapq.heappush(priority_queue, (total_cost, neighbor_city, path))
+
+#     return None, None
+
+
+
 import heapq
+import json
+
+
+# function to load json file into a dictionary
+def load_json(json_file):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+    return data
+
+graph_complete = load_json("graph_complete.json")
+# coordinates_graph = load_json("city_data.json")
+
+# import math
+
+# def haversine(lat1, lon1, lat2, lon2):
+#     R = 6371  # radius of Earth in kilometers
+#     dlat = math.radians(lat2 - lat1)
+#     dlon = math.radians(lon2 - lon1)
+#     a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) ** 2
+#     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+#     distance = R * c
+#     return distance
+
+# def heuristic(city, goal):
+#     x1, y1 = coordinates_graph[city]
+#     x2, y2 = coordinates_graph[goal]
+#     distance = haversine(x1, y1, x2, y2)
+#     return distance
+
+
+
+# heuristic function
+def heuristic(city_name, goal_name):
+    # graph_complete = load_json("graph_complete.json")
+    if city_name == goal_name:
+        return 0
+    for connection in graph_complete[city_name]:
+        if connection['city'] == goal_name:
+            return connection['distance']
+    # return None
+
+
+# # heuristic function
+# def heuristic(graph, start, goal):
+#     priority_queue = [(0, start)]
+#     visited = set()
+
+#     while priority_queue:
+#         (cost, current_node) = heapq.heappop(priority_queue)
+
+#         if current_node in visited:
+#             continue
+
+#         visited.add(current_node)
+
+#         if current_node == goal:
+#             return cost
+
+#         for neighbor in graph[current_node]:
+#             neighbor_city = neighbor['city']
+#             neighbor_cost = neighbor['distance']
+#             total_cost = cost + neighbor_cost
+#             heapq.heappush(priority_queue, (total_cost, neighbor_city))
+
+#     return float('inf')  # Indicates that there is no path between start and goal
+
 
 # a star search algorithm
+
+
+import heapq
+
+
+# A* search algorithm
 def a_star_search(graph, start, goal):
     priority_queue = [(0, start, [])]
     visited = set()
@@ -16,15 +117,80 @@ def a_star_search(graph, start, goal):
             continue
 
         visited.add(current_node)
-        path = path + [current_node]
 
         if current_node == goal:
-            return path, cost
+            return path + [current_node], cost
 
         for neighbor in graph[current_node]:
             neighbor_city = neighbor['city']
             neighbor_cost = neighbor['distance']
-            total_cost = cost + neighbor_cost
-            heapq.heappush(priority_queue, (total_cost, neighbor_city, path))
+            # total_cost = cost + neighbor_cost + heuristic(graph, neighbor_city, goal)
+            total_cost = cost + neighbor_cost + heuristic(neighbor_city, goal)
+            heapq.heappush(priority_queue, (total_cost, neighbor_city, path + [current_node]))  # Create a new list
 
     return None, None
+
+
+
+
+# def a_star_search(graph, start, goal):
+#     priority_queue = [(0, start, [])]
+#     visited = set()
+
+#     while priority_queue:
+#         (cost, current_node, path) = heapq.heappop(priority_queue)
+
+#         if current_node in visited:
+#             continue
+
+#         visited.add(current_node)
+#         path = path + [current_node]
+
+#         if current_node == goal:
+#             return path, cost
+
+#         for neighbor in graph[current_node]:
+#             neighbor_city = neighbor['city']
+#             neighbor_cost = neighbor['distance']
+#             total_cost = cost + neighbor_cost + heuristic(graph, neighbor_city, goal)  # Update with heuristic
+#             heapq.heappush(priority_queue, (total_cost, neighbor_city, path))
+
+#     return None, None
+
+
+
+
+# def a_star_search(graph, start, goal):
+#     def h(node):
+#         return 0
+
+#     def reconstruct_path(came_from, current):
+#         total_path = [current]
+#         while current in came_from:
+#             current = came_from[current]
+#             total_path.insert(0, current)
+#         return total_path
+
+#     open_set = {start}
+#     came_from = {}
+#     g_score = {node: float('inf') for node in graph}
+#     g_score[start] = 0
+#     f_score = {node: float('inf') for node in graph}
+#     f_score[start] = h(start)
+
+#     while open_set:
+#         current = min(open_set, key=lambda node: f_score[node])
+#         if current == goal:
+#             return reconstruct_path(came_from, current), g_score[goal]
+
+#         open_set.remove(current)
+#         for neighbor in graph[current]:
+#             tentative_g_score = g_score[current] + graph[current][neighbor]
+#             if tentative_g_score < g_score[neighbor]:
+#                 came_from[neighbor] = current
+#                 g_score[neighbor] = tentative_g_score
+#                 f_score[neighbor] = g_score[neighbor] + h(neighbor)
+#                 if neighbor not in open_set:
+#                     open_set.add(neighbor)
+
+#     return None, float('inf')  # No path found
--- dijkstra.py
+++ dijkstra.py
@@ -2,29 +2,101 @@
 
 # adapted from: https://www.udacity.com/blog/2021/10/implementing-dijkstras-algorithm-in-python.html
 
-import heapq
+# import heapq
 
-# Dijkstra's Algorithm
-def dijkstra_algorithm(graph, start, end):
-    priority_queue = [(0, start, [])]
-    visited = set()
+# # Dijkstra's Algorithm
+# def dijkstra_algorithm(graph, start, end):
+#     priority_queue = [(0, start, [])]
+#     visited = set()
 
-    while priority_queue:
-        (current_distance, current_city, path) = heapq.heappop(priority_queue)
+#     while priority_queue:
+#         (current_distance, current_city, path) = heapq.heappop(priority_queue)
 
-        if current_city in visited:
-            continue
+#         if current_city in visited:
+#             continue
 
-        visited.add(current_city)
-        path = path + [current_city]
+#         visited.add(current_city)
+#         path = path + [current_city]
 
-        if current_city == end:
-            return current_distance, path
+#         if current_city == end:
+#             return current_distance, path
 
-        for neighbor in graph[current_city]:
-            neighbor_city = neighbor['city']
-            neighbor_distance = neighbor['distance']
-            total_distance = current_distance + neighbor_distance
-            heapq.heappush(priority_queue, (total_distance, neighbor_city, path))
+#         for neighbor in graph[current_city]:
+#             neighbor_city = neighbor['city']
+#             neighbor_distance = neighbor['distance']
+#             total_distance = current_distance + neighbor_distance
+#             heapq.heappush(priority_queue, (total_distance, neighbor_city, path))
 
-    return float('inf'), []
+#     return float('inf'), []
+
+
+from queue import PriorityQueue
+
+# def dijkstra_algorithm(graph, start, goal):
+#     """
+#     Dijkstra's algorithm implementation.
+#     """
+#     # Initialize the open list with the start node and the closed list as empty
+#     open_list = PriorityQueue()
+#     open_list.put((0, start))
+#     came_from = {}  # Track the path
+#     cost_so_far = {start: 0}  # Cost from start to the node
+
+#     while not open_list.empty():
+#         # Get the current node with the lowest cost
+#         current = open_list.get()[1]
+
+#         # If the goal is reached, reconstruct and return the path
+#         if current == goal:
+#             path = []
+#             while current in came_from:
+#                 path.append(current)
+#                 current = came_from[current]
+#             path.append(start)
+#             return path[::-1], cost_so_far[goal]  # Path and cost
+
+#         # Explore neighbors
+#         for neighbor in graph[current]:
+#             new_cost = cost_so_far[current] + neighbor['distance']
+#             if neighbor['city'] not in cost_so_far or new_cost < cost_so_far[neighbor['city']]:
+#                 cost_so_far[neighbor['city']] = new_cost
+#                 open_list.put((new_cost, neighbor['city']))
+#                 came_from[neighbor['city']] = current
+
+#     return None, None  # Return None if there is no path to the goal
+
+
+def dijkstra_algorithm(graph, start, goal):
+    open_list = PriorityQueue()
+    open_list.put((0, start))
+    came_from = {}
+    cost_so_far = {start: 0}
+    nodes_explored = 0
+
+    while not open_list.empty():
+        current = open_list.get()[1]
+        nodes_explored += 1
+
+        if current == goal:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.append(start)
+            return path[::-1], cost_so_far[goal], nodes_explored
+
+        for neighbor in graph[current]:
+            new_cost = cost_so_far[current] + neighbor['distance']
+            if neighbor['city'] not in cost_so_far or new_cost < cost_so_far[neighbor['city']]:
+                cost_so_far[neighbor['city']] = new_cost
+                open_list.put((new_cost, neighbor['city']))
+                came_from[neighbor['city']] = current
+
+    return None, None, None
+
+
+# # Example usage of the Dijkstra algorithm
+# start_city = 'Karachi'
+# goal_city = 'Lahore'
+# path, cost = dijkstra_search(graph_data, start_city, goal_city)
+# path, cost
