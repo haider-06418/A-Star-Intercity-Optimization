86b7d26be5334a8155a29e651f125e7356497582
haider-06418 <ma06418@st.habib.edu.pk>
2023-12-12 23:00:24 +0500

    corrected implmentation for bestfs

--- bestfs.py
+++ bestfs.py
@@ -20,32 +20,31 @@ def heuristic(node, goal, graph):
 def best_first_search(graph, start, goal):
     open_list = PriorityQueue()
     open_list.put((heuristic(start, goal, graph), start))
-    came_from = {}
-    cost_so_far = {start: 0}
+    came_from = {}  # Track the path
+    cost_so_far = {start: 0}  # Cost from start to the node
     nodes_explored = 0
 
     while not open_list.empty():
+        # Get the current node with the lowest heuristic value
         current = open_list.get()[1]
         nodes_explored += 1
 
+        # If the goal is reached, reconstruct and return the path
         if current == goal:
             path = []
             while current in came_from:
                 path.append(current)
                 current = came_from[current]
-
-                came_from.pop(current)
-
             path.append(start)
+            return path[::-1], cost_so_far[goal], nodes_explored  # Path, cost, and nodes explored
 
-            return path[::-1], cost_so_far[goal], nodes_explored
-
+        # Explore neighbors
         for neighbor in graph[current]:
-            if neighbor['city'] not in came_from:
-                new_cost = cost_so_far[current] + neighbor['distance']
+            new_cost = cost_so_far[current] + neighbor['distance']
+            if neighbor['city'] not in cost_so_far or new_cost < cost_so_far[neighbor['city']]:
                 cost_so_far[neighbor['city']] = new_cost
                 priority = heuristic(neighbor['city'], goal, graph)
                 open_list.put((priority, neighbor['city']))
                 came_from[neighbor['city']] = current
 
-    return None, None, nodes_explored
+    return None, None, None  # Return None if there is no path to the goal
