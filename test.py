2b1580ea09c8e985fa1fa0b6ccb30062abd02e6f
haider-06418 <ma06418@st.habib.edu.pk>
2023-12-12 22:59:43 +0500

    testing for bestfs

--- test.py
+++ test.py
@@ -83,3 +83,41 @@ sum_djkistra += heuristic(graph, 'Chiniot', 'Mandi Bahauddin')
 sum_djkistra += heuristic(graph, 'Mandi Bahauddin', 'Islamabad')
 
 print(sum_djkistra)
+
+
+# testing best first
+import json
+import time
+from bestfs import best_first_search
+def load_json(json_file):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+    return data
+
+graph = load_json('graphs/graph_updated.json')
+
+# start_city = "Karachi"
+# goal_city = "Mirpur Khas"
+
+# start_city = "Jhelum"
+# goal_city = "Gwadar"
+
+start_city = "Gujrat"
+goal_city = "Gilgit"
+
+
+# best first search
+start_time = time.time()
+path_3, cost_3, no_of_nodes_explored = best_first_search(graph, start_city, goal_city)
+end_time = time.time()
+time_taken = end_time - start_time
+print('BestFS:')
+if path_3:
+    print(f"Path from {start_city} to {goal_city}:")
+    print(" -> ".join(path_3))
+    print(f"Total Cost: {cost_3}")
+    print(f"Number of nodes explored: {no_of_nodes_explored}")
+    print(f"Time taken: {time_taken} seconds")
+else:
+    print(f"No path found from {start_city} to {goal_city}.")
+print(' ')
